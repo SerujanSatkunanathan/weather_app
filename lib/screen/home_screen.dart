51b2a8d72fc125134abb052a334c08b2e304309d
SerujanSatkunanathan <144580005+SerujanSatkunanathan@users.noreply.github.com>
2024-08-29 11:22:35 +0530

    Fully completed app

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:ui';
+import 'package:intl/intl.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -13,6 +14,34 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Widget getWetherIcon(int code) {
+    switch (code) {
+      case > 200 && <= 300:
+        return Image.asset('assets/6.png');
+
+      case > 300 && <= 400:
+        return Image.asset('assets/7.png');
+
+      case > 500 && <= 600:
+        return Image.asset('assets/2.png');
+
+      case > 600 && <= 700:
+        return Image.asset('assets/4.png');
+
+      case > 700 && <= 800:
+        return Image.asset('assets/9.png');
+
+      case == 800:
+        return Image.asset('assets/1.png');
+
+      case > 800 && <= 804:
+        return Image.asset('assets/13.png');
+
+      default:
+        return Image.asset('assets/11.png');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,53 +50,54 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        systemOverlayStyle: SystemUiOverlayStyle(
+        systemOverlayStyle: const SystemUiOverlayStyle(
           statusBarBrightness: Brightness.dark,
         ),
       ),
       body: Padding(
-        padding: EdgeInsets.fromLTRB(40, 1.2 * kToolbarHeight, 40, 20),
+        padding: const EdgeInsets.fromLTRB(40, 1.2 * kToolbarHeight, 40, 20),
         child: SizedBox(
           height: MediaQuery.of(context).size.height,
           child: Stack(
             children: [
               Align(
-                alignment: AlignmentDirectional(3, -0.3),
+                alignment: const AlignmentDirectional(3, -0.3),
                 child: Container(
                   height: 300,
                   width: 300,
-                  decoration: BoxDecoration(
+                  decoration: const BoxDecoration(
                       shape: BoxShape.circle,
-                      color: const Color.fromARGB(255, 31, 224, 208)),
+                      color: Color.fromARGB(255, 31, 224, 208)),
                 ),
               ),
               Align(
-                alignment: AlignmentDirectional(-3, -0.2),
+                alignment: const AlignmentDirectional(-3, -0.2),
                 child: Container(
                   height: 300,
                   width: 300,
-                  decoration: BoxDecoration(
+                  decoration: const BoxDecoration(
                       shape: BoxShape.circle,
-                      color: const Color.fromARGB(255, 18, 201, 233)),
+                      color: Color.fromARGB(255, 18, 201, 233)),
                 ),
               ),
               Align(
-                alignment: AlignmentDirectional(0, -1.2),
+                alignment: const AlignmentDirectional(0, -1.2),
                 child: Container(
                   height: 300,
                   width: 300,
-                  decoration: BoxDecoration(
-                      color: const Color.fromARGB(255, 252, 220, 44)),
+                  decoration: const BoxDecoration(
+                      color: Color.fromARGB(255, 252, 220, 44)),
                 ),
               ),
               BackdropFilter(
                 filter: ImageFilter.blur(sigmaX: 100.0, sigmaY: 100.0),
                 child: Container(
-                  decoration: BoxDecoration(color: Colors.transparent),
+                  decoration: const BoxDecoration(color: Colors.transparent),
                 ),
               ),
               BlocBuilder<WeatherBlocBloc, WeatherBlocState>(
-                builder: (context, state) {
+                  builder: (context, state) {
+                if (state is WeatherBlocSuccess) {
                   return SizedBox(
                     width: MediaQuery.of(context).size.width,
                     height: MediaQuery.of(context).size.height,
@@ -75,16 +105,16 @@ class _HomeScreenState extends State<HomeScreen> {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                          '📌 Colombo Srilanka',
-                          style: TextStyle(
+                          '📌 ${state.weather.areaName}',
+                          style: const TextStyle(
                             color: Colors.white,
                             fontSize: 16,
                           ),
                         ),
-                        SizedBox(
+                        const SizedBox(
                           height: 8,
                         ),
-                        Text(
+                        const Text(
                           "Good Morning",
                           style: TextStyle(
                             color: Colors.white,
@@ -92,17 +122,24 @@ class _HomeScreenState extends State<HomeScreen> {
                             fontWeight: FontWeight.bold,
                           ),
                         ),
-                        Image.asset('assets/11.png'),
-                        const Center(
+                        Center(
+                          child: getWetherIcon(
+                              state.weather.weatherConditionCode!),
+                        ),
+                        Center(
                           child: Text(
-                            '21°C',
-                            style: TextStyle(fontSize: 40, color: Colors.white),
+                            '${state.weather.temperature!.celsius!.round()}°C',
+                            style: const TextStyle(
+                              fontSize: 45,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
-                        const Center(
+                        Center(
                           child: Text(
-                            'TUNDERSTORM',
-                            style: TextStyle(
+                            state.weather.weatherMain!.toUpperCase(),
+                            style: const TextStyle(
                                 fontSize: 28,
                                 color: Colors.white,
                                 fontWeight: FontWeight.bold),
@@ -111,16 +148,19 @@ class _HomeScreenState extends State<HomeScreen> {
                         const SizedBox(
                           height: 5,
                         ),
-                        const Center(
+                        Center(
                           child: Text(
-                            'Friday 16 ◉ 9.57am ',
-                            style: TextStyle(
+                            DateFormat("EEEE dd ◉")
+                                .add_jm()
+                                .format(state.weather.date!),
+                            //'Friday 16 ◉ 9.57am ',
+                            style: const TextStyle(
                               fontSize: 20,
                               color: Colors.white,
                             ),
                           ),
                         ),
-                        SizedBox(
+                        const SizedBox(
                           height: 20,
                         ),
                         Center(
@@ -135,28 +175,31 @@ class _HomeScreenState extends State<HomeScreen> {
                                       child: Image.asset(
                                         'assets/1.png',
                                       )),
-                                  SizedBox(
+                                  const SizedBox(
                                     width: 3,
                                   ),
-                                  const Column(
+                                  Column(
                                     children: [
-                                      Text(
+                                      const Text(
                                         "Sunrise",
                                         style: TextStyle(
                                           color: Colors.white,
                                         ),
                                       ),
                                       Text(
-                                        "5.55am",
-                                        style: TextStyle(
+                                        DateFormat()
+                                            .add_jm()
+                                            .format(state.weather.sunrise!),
+                                        // "5.55am",
+                                        style: const TextStyle(
                                           color: Colors.white,
                                           fontWeight: FontWeight.bold,
                                         ),
                                       )
                                     ],
                                   ),
-                                  SizedBox(
-                                    width: 120,
+                                  const SizedBox(
+                                    width: 100,
                                   ),
                                   SizedBox(
                                       height: 50,
@@ -164,20 +207,23 @@ class _HomeScreenState extends State<HomeScreen> {
                                       child: Image.asset(
                                         'assets/14.png',
                                       )),
-                                  SizedBox(
+                                  const SizedBox(
                                     width: 3,
                                   ),
-                                  const Column(
+                                  Column(
                                     children: [
-                                      Text(
-                                        "Sunrise",
+                                      const Text(
+                                        "Sunset",
                                         style: TextStyle(
                                           color: Colors.white,
                                         ),
                                       ),
                                       Text(
-                                        "5.55am",
-                                        style: TextStyle(
+                                        DateFormat()
+                                            .add_jm()
+                                            .format(state.weather.sunset!),
+                                        //"5.55am",
+                                        style: const TextStyle(
                                           color: Colors.white,
                                           fontWeight: FontWeight.bold,
                                         ),
@@ -189,13 +235,13 @@ class _HomeScreenState extends State<HomeScreen> {
                             ],
                           ),
                         ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 5),
+                        const Padding(
+                          padding: EdgeInsets.symmetric(vertical: 5),
                           child: Divider(
                             color: Colors.white54,
                           ),
                         ),
-                        SizedBox(
+                        const SizedBox(
                           height: 5,
                         ),
                         Center(
@@ -210,28 +256,30 @@ class _HomeScreenState extends State<HomeScreen> {
                                       child: Image.asset(
                                         'assets/5.png',
                                       )),
-                                  SizedBox(
+                                  const SizedBox(
                                     width: 3,
                                   ),
-                                  const Column(
+                                  Column(
                                     children: [
-                                      Text(
-                                        "Sunrise",
+                                      const Text(
+                                        "Temp Max",
                                         style: TextStyle(
                                           color: Colors.white,
                                         ),
                                       ),
                                       Text(
-                                        "5.55am",
-                                        style: TextStyle(
+                                        '${state.weather.tempMax!.celsius!.round().toString()}°C',
+
+                                        //"5.55am",
+                                        style: const TextStyle(
                                           color: Colors.white,
                                           fontWeight: FontWeight.bold,
                                         ),
                                       )
                                     ],
                                   ),
-                                  SizedBox(
-                                    width: 120,
+                                  const SizedBox(
+                                    width: 90,
                                   ),
                                   SizedBox(
                                       height: 50,
@@ -239,20 +287,20 @@ class _HomeScreenState extends State<HomeScreen> {
                                       child: Image.asset(
                                         'assets/8.png',
                                       )),
-                                  SizedBox(
+                                  const SizedBox(
                                     width: 3,
                                   ),
-                                  const Column(
+                                  Column(
                                     children: [
-                                      Text(
-                                        "Sunrise",
+                                      const Text(
+                                        "Temp Min",
                                         style: TextStyle(
                                           color: Colors.white,
                                         ),
                                       ),
                                       Text(
-                                        "5.55am",
-                                        style: TextStyle(
+                                        '${state.weather.tempMin!.celsius!.round().toString()}°C',
+                                        style: const TextStyle(
                                           color: Colors.white,
                                           fontWeight: FontWeight.bold,
                                         ),
@@ -267,8 +315,10 @@ class _HomeScreenState extends State<HomeScreen> {
                       ],
                     ),
                   );
-                },
-              )
+                } else {
+                  return Container();
+                }
+              })
             ],
           ),
         ),
